c17464ee6c28d9afa2c047d176dbb12289051e3f
sanex3339 <sanex3339@yandex.ru>
2016-05-14 00:36:02 +0300

    0.2.1

---
+++ typings/browser.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="browser/ambient/esprima/index.d.ts" />
+/// <reference path="browser/ambient/estree/index.d.ts" />
+/// <reference path="browser/ambient/node/index.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="main/ambient/esprima/index.d.ts" />
+/// <reference path="main/ambient/estree/index.d.ts" />
+/// <reference path="main/ambient/node/index.d.ts" />

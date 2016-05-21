c3bea52f739456892de9fb2bcec4de45baeaddeb
sanex3339 <sanex3339@yandex.ru>
2016-05-21 16:01:09 +0300

    formatting

--- src/Utils.ts
+++ src/Utils.ts
@@ -81,7 +81,7 @@ export class Utils {
      * @param obj
      * @returns {T}
      */
-    public static strEnumify <T extends { [prop: string]: '' | string }> (obj: T): T {
+    public static strEnumify <T extends {[prop: string]: ''|string}> (obj: T): T {
         return obj;
     }
 

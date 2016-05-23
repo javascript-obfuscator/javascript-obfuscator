aa0347e8c8e8964c2dbb7db90c1f733f76b62188
sanex3339 <sanex3339@yandex.ru>
2016-05-23 16:26:45 +0300

    refactoring

--- src/nodes/ConsoleOutputDisableExpressionNode.js
+++ src/nodes/ConsoleOutputDisableExpressionNode.js
@@ -23,11 +23,15 @@ class ConsoleOutputDisableExpressionNode extends Node_1.Node {
     getNodeStructure() {
         return NodeUtils_1.NodeUtils.getBlockScopeNodeByIndex(esprima.parse(`
                 (function () {
-                    var _ = '(\u0004\u0006\u0003\u0005[]["filter"]["\u0007tructor"]("return this")().\u0003;\u0006\u0002\u0005\u0004};_\u0003.log\u0001.info\u0001.warn\u0001.error\u0001})();\u0001\u0005_\u0002;_\u0003\u0002function\u0003\u0007ole\u0004\u0002 () {\u0005 = \u0006var _\u0007cons', Y, $;
+                    var _ = '(\u0004\u0006\u0003\u0005[]["filter"]["\u0007tructor"]("return this")().\u0003;\u0006\u0002\u0005\u0004};_\u0003.log\u0001.info\u0001.warn\u0001.error\u0001})();\u0001\u0005_\u0002;_\u0003\u0002function\u0003\u0007ole\u0004\u0002 () {\u0005 = \u0006var _\u0007cons', 
+                        Y, 
+                        $;
+                    
                     for (Y in $ = "\u0007\u0006\u0005\u0004\u0003\u0002\u0001") {
                       var arr = _.split($[Y]);
                       _ = arr.join(arr.pop());
                     }
+                    
                     []["filter"]["constructor"](_)();
                 })()
             `));

5605fa274c7dc422a6df6b4e35d60d512e8bc643
sanex3339 <sanex3339@yandex.ru>
2016-05-23 16:57:52 +0300

    refactoring

--- src/nodes/ConsoleOutputDisableExpressionNode.js
+++ src/nodes/ConsoleOutputDisableExpressionNode.js
@@ -23,7 +23,7 @@ class ConsoleOutputDisableExpressionNode extends Node_1.Node {
     getNodeStructure() {
         return NodeUtils_1.NodeUtils.getBlockScopeNodeByIndex(esprima.parse(`
                 (function () {
-                    var _ = '(\u0004\u0006\u0003\u0005[]["filter"]["\u0007tructor"]("return this")().\u0003;\u0006\u0002\u0005\u0004};_\u0003.log\u0001.info\u0001.warn\u0001.error\u0001})();\u0001\u0005_\u0002;_\u0003\u0002function\u0003\u0007ole\u0004\u0002 () {\u0005 = \u0006var _\u0007cons', 
+                    var _ = '(\u0004\u0006\u0003\u0005[]' + '["filter"]["\u0007tructor"]' + '("return this")()' + '.' + '\u0003;\u0006\u0002\u0005\u0004};' + '_\u0003.log\u0001.in' + 'fo\u0001.' + 'war' + 'n\u0001.er' + 'r' + 'or\u0001})();' + '\u0001\u0005_\u0002;' + '_\u0003\u0002function' + '\u0003\u0007ole\u0004\u0002 ()' + '{\u0005 = \u0006var ' + '_\u0007cons', 
                         Y, 
                         $;
                     

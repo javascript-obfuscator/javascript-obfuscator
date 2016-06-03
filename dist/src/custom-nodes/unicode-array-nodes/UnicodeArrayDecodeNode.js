e2c92db02660500d1deb5a92ffdc1c5f2f6966a2
sanex3339 <sanex3339@yandex.ru>
2016-06-03 22:37:38 +0300

    0.5.3 atob polyfill fix

--- dist/src/custom-nodes/unicode-array-nodes/UnicodeArrayDecodeNode.js
+++ dist/src/custom-nodes/unicode-array-nodes/UnicodeArrayDecodeNode.js
@@ -28,22 +28,24 @@ class UnicodeArrayDecodeNode extends Node_1.Node {
         let node = esprima.parse(`
             (function () {
                 ${JavaScriptObfuscator_1.JavaScriptObfuscator.obfuscate(`
-                    var object = []['filter']['constructor']('return this')();
-                    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-        
-                    object.atob || (
-                        object.atob = function(input) {
-                            var str = String(input).replace(/=+$/, '');
-                            for (
-                                var bc = 0, bs, buffer, idx = 0, output = '';
-                                buffer = str.charAt(idx++);
-                                ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
-                                    bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
-                            ) {
-                                buffer = chars.indexOf(buffer);
-                            }
-                        return output;
-                    });
+                    (function () {
+                        var object = []['filter']['constructor']('return this')();
+                        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+            
+                        object.atob || (
+                            object.atob = function(input) {
+                                var str = String(input).replace(/=+$/, '');
+                                for (
+                                    var bc = 0, bs, buffer, idx = 0, output = '';
+                                    buffer = str.charAt(idx++);
+                                    ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
+                                        bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
+                                ) {
+                                    buffer = chars.indexOf(buffer);
+                                }
+                            return output;
+                        });
+                    })();
                 `, NoCustomNodesPreset_1.NO_CUSTOM_NODES_PRESET)}
               
                 var ${tempArrayName} = [];

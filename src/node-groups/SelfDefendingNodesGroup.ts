a40c65e64a344a08753907061ea87b362aa11651
sanex3339 <sanex3339@yandex.ru>
2016-06-04 18:36:12 +0300

    selfDefending test implementation

--- src/node-groups/SelfDefendingNodesGroup.ts
+++ src/node-groups/SelfDefendingNodesGroup.ts
@@ -1,12 +1,7 @@
 import { IOptions } from "../interfaces/IOptions";
 
-import { DebugProtectionFunctionCallNode } from "../custom-nodes/debug-protection-nodes/DebugProtectionFunctionCallNode";
-import { DebugProtectionFunctionIntervalNode } from "../custom-nodes/debug-protection-nodes/DebugProtectionFunctionIntervalNode";
-import { DebugProtectionFunctionNode } from "../custom-nodes/debug-protection-nodes/DebugProtectionFunctionNode";
-
 import { NodesGroup } from './NodesGroup';
-import { Utils } from '../Utils';
-import {SelfDefendingUnicodeNode} from "../custom-nodes/self-defending-nodes/SelfDefendingUnicodeNode";
+import { SelfDefendingUnicodeNode } from "../custom-nodes/self-defending-nodes/SelfDefendingUnicodeNode";
 
 export class SelfDefendingNodesGroup extends NodesGroup {
     /**

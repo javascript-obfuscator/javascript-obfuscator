f77c02af8d2452f03b009444f832d92bd3e68bb0
sanex3339 <sanex3339@yandex.ru>
2016-06-16 16:16:22 +0300

    utils tests

---
+++ test/OptionsNormalizer.spec.ts
@@ -0,0 +1,42 @@
+import { IOptionsPreset } from "../src/interfaces/IOptionsPreset";
+
+import { OptionsNormalizer } from '../src/OptionsNormalizer';
+
+import { DEFAULT_PRESET } from '../src/preset-options/DefaultPreset';
+
+let assert: any = require('chai').assert;
+
+describe('OptionsNormalizer', () => {
+    describe('normalizeOptionsPreset (options: IOptionsPreset): IOptionsPreset', () => {
+        let optionsPreset1: IOptionsPreset = Object.assign({}, DEFAULT_PRESET, {
+                compact: false,
+                rotateUnicodeArray: true,
+                unicodeArray: false,
+                unicodeArrayThreshold: 0.5,
+                wrapUnicodeArrayCalls: true
+
+            }),
+            optionsPreset2: IOptionsPreset = Object.assign({}, DEFAULT_PRESET, {
+                unicodeArrayThreshold: 2
+
+            });
+
+        it('should normalize options preset', () => {
+            assert.deepEqual(
+                OptionsNormalizer.normalizeOptionsPreset(optionsPreset1), Object.assign({}, DEFAULT_PRESET, {
+                    compact: true,
+                    rotateUnicodeArray: false,
+                    unicodeArray: false,
+                    unicodeArrayThreshold: 0,
+                    wrapUnicodeArrayCalls: false
+                })
+            );
+
+            assert.deepEqual(
+                OptionsNormalizer.normalizeOptionsPreset(optionsPreset2), Object.assign({}, DEFAULT_PRESET, {
+                    unicodeArrayThreshold: 1
+                })
+            );
+        });
+    });
+});

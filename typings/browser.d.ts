47359f5e8d52300bad10fa840a8a5513697b3ee5
sanex3339 <sanex3339@yandex.ru>
2016-06-16 13:17:54 +0300

    tests initialization

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,5 +1,7 @@
+/// <reference path="browser/ambient/chai/index.d.ts" />
 /// <reference path="browser/ambient/escodegen/index.d.ts" />
 /// <reference path="browser/ambient/esprima/index.d.ts" />
 /// <reference path="browser/ambient/estraverse/index.d.ts" />
 /// <reference path="browser/ambient/estree/index.d.ts" />
+/// <reference path="browser/ambient/mocha/index.d.ts" />
 /// <reference path="browser/ambient/node/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,5 +1,7 @@
+/// <reference path="main/ambient/chai/index.d.ts" />
 /// <reference path="main/ambient/escodegen/index.d.ts" />
 /// <reference path="main/ambient/esprima/index.d.ts" />
 /// <reference path="main/ambient/estraverse/index.d.ts" />
 /// <reference path="main/ambient/estree/index.d.ts" />
+/// <reference path="main/ambient/mocha/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />

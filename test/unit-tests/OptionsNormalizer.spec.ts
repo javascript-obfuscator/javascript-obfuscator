821b7d06bbe20e19b8eaa5cb12d9906477a07207
sanex3339 <sanex3339@yandex.ru>
2016-07-13 13:58:11 +0300

    options validation with Joi tests update

--- test/unit-tests/OptionsNormalizer.spec.ts
+++ test/unit-tests/OptionsNormalizer.spec.ts
@@ -8,7 +8,8 @@ const assert: Chai.AssertStatic = require('chai').assert;
 
 describe('OptionsNormalizer', () => {
     describe('normalizeOptions (options: IObfuscatorOptions): IObfuscatorOptions', () => {
-        let optionsPreset1: IObfuscatorOptions;
+        let optionsPreset1: IObfuscatorOptions,
+            optionsPreset2: IObfuscatorOptions;
 
         beforeEach(() => {
             optionsPreset1 = Object.assign({}, DEFAULT_PRESET, {
@@ -18,12 +19,27 @@ describe('OptionsNormalizer', () => {
                 unicodeArrayThreshold: 0.5,
                 wrapUnicodeArrayCalls: true
             });
+            optionsPreset2 = Object.assign({}, DEFAULT_PRESET, {
+                rotateUnicodeArray: true,
+                unicodeArray: true,
+                unicodeArrayThreshold: 0,
+                wrapUnicodeArrayCalls: true
+            });
         });
 
         it('should normalize options preset', () => {
             assert.deepEqual(
                 OptionsNormalizer.normalizeOptions(optionsPreset1), Object.assign({}, DEFAULT_PRESET, {
                     compact: true,
+                    rotateUnicodeArray: false,
+                    unicodeArray: false,
+                    unicodeArrayThreshold: 0,
+                    wrapUnicodeArrayCalls: false
+                })
+            );
+
+            assert.deepEqual(
+                OptionsNormalizer.normalizeOptions(optionsPreset2), Object.assign({}, DEFAULT_PRESET, {
                     rotateUnicodeArray: false,
                     unicodeArray: false,
                     unicodeArrayThreshold: 0,

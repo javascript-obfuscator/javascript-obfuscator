83516b0185d27324f9f62c63ef56d760308844d2
sanex3339 <sanex3339@yandex.ru>
2016-10-04 15:28:53 +0300

    dinner fix

--- test/performance-tests/JavaScriptObfuscatorPerformance.spec.ts
+++ test/performance-tests/JavaScriptObfuscatorPerformance.spec.ts
@@ -8,10 +8,10 @@ describe('JavaScriptObfuscator performance', () => {
     const iterationsCount: number = 500;
 
     describe('performance', () => {
-        it('should returns object with obfuscated code and empty source map', function () {
+        it('should returns object with obfuscated code and empty source map', function (): void {
             this.timeout(15000);
 
-            for (let i = 0; i < iterationsCount; i++) {
+            for (let i: number = 0; i < iterationsCount; i++) {
                 JavaScriptObfuscator.obfuscate(readFileAsString('./test/fixtures/sample.js'));
             }
 

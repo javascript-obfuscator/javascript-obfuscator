8a43bb3ed7f94f18d8277c68fa2c4ecfb9c40de6
sanex3339 <sanex3339@yandex.ru>
2016-10-05 08:39:46 +0300

    increase timeout limit for performance tests

--- test/performance-tests/JavaScriptObfuscatorPerformance.spec.ts
+++ test/performance-tests/JavaScriptObfuscatorPerformance.spec.ts
@@ -9,7 +9,7 @@ describe('JavaScriptObfuscator performance', () => {
 
     describe('performance: multiple calls', () => {
         it('shows performance time with multiple obfuscator calls', function (): void {
-            this.timeout(10000);
+            this.timeout(100000);
 
             for (let i: number = 0; i < iterationsCount; i++) {
                 JavaScriptObfuscator.obfuscate(readFileAsString('./test/fixtures/sample.js'));
@@ -21,7 +21,7 @@ describe('JavaScriptObfuscator performance', () => {
 
     describe('performance: large source code', () => {
         it('shows performance time with large code size', function (): void {
-            this.timeout(5000);
+            this.timeout(100000);
 
             JavaScriptObfuscator.obfuscate(readFileAsString('./test/fixtures/sample.js').repeat(iterationsCount));
 

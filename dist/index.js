9e2b4bb92fd6792ea1b8c8113abbe76979892847
sanex3339 <sanex3339@yandex.ru>
2016-11-09 17:07:06 +0300

    CatchClauseObfuscator.spec refactoring

--- dist/index.js
+++ dist/index.js
@@ -88,7 +88,7 @@ module.exports =
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 87);
+/******/ 	return __webpack_require__(__webpack_require__.s = 86);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -4333,8 +4333,7 @@ module.exports = require("fs");
 module.exports = require("mkdirp");
 
 /***/ },
-/* 86 */,
-/* 87 */
+/* 86 */
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";

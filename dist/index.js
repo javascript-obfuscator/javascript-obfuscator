ce2391cb2fbfe9577ecedb119ec2cdc165c9fa13
sanex3339 <sanex3339@yandex.ru>
2016-11-19 19:53:25 +0300

    0.8.1

--- dist/index.js
+++ dist/index.js
@@ -88,7 +88,7 @@ module.exports =
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 89);
+/******/ 	return __webpack_require__(__webpack_require__.s = 87);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -4431,9 +4431,7 @@ module.exports = require("fs");
 module.exports = require("mkdirp");
 
 /***/ },
-/* 87 */,
-/* 88 */,
-/* 89 */
+/* 87 */
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";

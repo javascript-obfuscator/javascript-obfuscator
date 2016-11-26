e5f2478c383eec42f92805b493edd41f06cee062
sanex3339 <sanex3339@yandex.ru>
2016-11-26 16:40:32 +0300

    fixed interface

--- src/interfaces/IObfuscator.d.ts
+++ src/interfaces/IObfuscator.d.ts
@@ -1,13 +1,5 @@
 import * as ESTree from 'estree';
 
-import { ICustomNode } from './custom-nodes/ICustomNode';
-import { IObfuscationEventEmitter } from './IObfuscationEventEmitter';
-import { IStorage } from './IStorage';
-
 export interface IObfuscator {
-    obfuscateAstTree (
-        astTree: ESTree.Program,
-        obfuscationEventEmitter: IObfuscationEventEmitter,
-        customNodesStorage: IStorage<ICustomNode>
-    ): ESTree.Program;
+    obfuscateAstTree (astTree: ESTree.Program): ESTree.Program;
 }

d02d09be83247c4aa97453e1765acbcc8f7253f3
sanex3339 <sanex3339@yandex.ru>
2016-11-27 00:04:53 +0300

    inversify test

--- src/interfaces/IContainerServiceIdentifiers.d.ts
+++ src/interfaces/IContainerServiceIdentifiers.d.ts
@@ -1,10 +1,10 @@
 import { interfaces } from 'inversify';
 
 export interface IContainerServiceIdentifiers {
-    IObfuscationEventEmitter: interfaces.ServiceIdentifier<any>,
-    IObfuscator: interfaces.ServiceIdentifier<any>,
-    IOptions: interfaces.ServiceIdentifier<any>,
-    IStackTraceAnalyzer: interfaces.ServiceIdentifier<any>,
-    IStorage: interfaces.ServiceIdentifier<any>,
-    [key: string]: interfaces.ServiceIdentifier<any>
+    IObfuscationEventEmitter: interfaces.ServiceIdentifier<any>;
+    IObfuscator: interfaces.ServiceIdentifier<any>;
+    IOptions: interfaces.ServiceIdentifier<any>;
+    IStackTraceAnalyzer: interfaces.ServiceIdentifier<any>;
+    IStorage: interfaces.ServiceIdentifier<any>;
+    [key: string]: interfaces.ServiceIdentifier<any>;
 }

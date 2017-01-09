5d649b23d74e27d299afef289689714da60357b4
sanex3339 <sanex3339@yandex.ru>
2017-01-09 21:41:31 +0300

    syncing changes from 0.8.4 with dev

--- src/templates/custom-nodes/console-output-nodes/console-output-disable-expression-node/ConsoleOutputDisableExpressionTemplate.ts
+++ src/templates/custom-nodes/console-output-nodes/console-output-disable-expression-node/ConsoleOutputDisableExpressionTemplate.ts
@@ -9,10 +9,29 @@ export function ConsoleOutputDisableExpressionTemplate (): string {
             var func = function () {};
             var that = getGlobal();
                         
-            that.console.log = func; 
-            that.console.error = func;
-            that.console.warn = func;
-            that.console.info = func;
+            if (!that.console) {
+                that.console = (function (func){ 
+                    var c = {}; 
+                    
+                    c.log = func;
+                    c.warn = func; 
+                    c.debug = func;
+                    c.info = func;
+                    c.error = func;
+                    c.exception = func;
+                    c.trace = func;
+                    
+                    return c; 
+                })(func);
+            } else {
+                that.console.log = func;
+                that.console.warn = func; 
+                that.console.debug = func;
+                that.console.info = func;
+                that.console.error = func;
+                that.console.exception = func;
+                that.console.trace = func;
+            }
         });
         
         {consoleLogDisableFunctionName}();

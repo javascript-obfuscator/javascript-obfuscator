e3066b41890588b947dbaa514bae1dd59ab19c9b
sanex3339 <sanex3339@yandex.ru>
2017-09-13 01:35:23 +0300

    Fixed debug protection template false positive alert

--- src/templates/custom-nodes/debug-protection-nodes/debug-protection-function-call-node/DebufProtectionFunctionCallTemplate.ts
+++ src/templates/custom-nodes/debug-protection-nodes/debug-protection-function-call-node/DebufProtectionFunctionCallTemplate.ts
@@ -3,14 +3,16 @@
  */
 export function DebugProtectionFunctionCallTemplate (): string {
     return `
-        var regExp1 = new RegExp('function *\\\\( *\\\\)');
-        var regExp2 = new RegExp('_0x([a-f0-9]){4,6} *\\\\(\\\\+\\\\+ *_0x([a-f0-9]){4,6} *\\\\)');
-        var result = {debugProtectionFunctionName}('init');
-        
-        if (!regExp1.test(result + 'chain') || !regExp2.test(result + 'input')) {
-            result('0');
-        } else {
-            {debugProtectionFunctionName}();
-        }
+        (function () {
+            var regExp1 = new RegExp('function *\\\\( *\\\\)');
+            var regExp2 = new RegExp('\\\\+\\\\+ *_0x([a-f0-9]){4,6}');
+            var result = {debugProtectionFunctionName}('init');
+            
+            if (!regExp1.test(result + 'chain') || !regExp2.test(result + 'input')) {
+                result('0');
+            } else {
+                {debugProtectionFunctionName}();
+            }
+        })();
     `;
 }

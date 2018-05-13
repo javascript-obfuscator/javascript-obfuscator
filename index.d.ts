a673964d2e9251ede07d59871e519d13121fb667
sanex3339 <sanex3339@yandex.ru>
2018-05-13 20:53:30 +0300

    Dependencies update

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import { TInputOptions } from './src/types/options/TInputOptions';
+
+import { IObfuscationResult } from './src/interfaces/IObfuscationResult';
+
+export function obfuscate (sourceCode: string, inputOptions?: TInputOptions): IObfuscationResult

594d8c32bf4b87398e79de6307861c42f3c8b754
sanex3339 <sanex3339@yandex.ru>
2018-06-02 00:13:53 +0300

    Dependencies update: typescript 2.9.1

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,8 @@ import { TInputOptions } from './src/types/options/TInputOptions';
 
 import { IObfuscationResult } from './src/interfaces/IObfuscationResult';
 
-export function obfuscate (sourceCode: string, inputOptions?: TInputOptions): IObfuscationResult
+export type ObfuscatorOptions = TInputOptions;
+
+export interface ObfuscationResult extends IObfuscationResult {}
+
+export function obfuscate (sourceCode: string, inputOptions?: ObfuscatorOptions): ObfuscationResult;

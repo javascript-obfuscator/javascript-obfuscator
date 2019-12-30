2a8e9608a34db22cb1aa6bd640783e84119b30dd
sanex3339 <yarabotayuvyandex3339>
2019-12-31 01:01:26 +0300

    Fixed `reservedNames` option

---
+++ test/functional-tests/node-transformers/obfuscating-transformers/literal-transformer/obfuscating-replacers/identifier-obfuscating-replacers/BaseIdentifierObfuscatingReplacer.spec.ts
@@ -0,0 +1,34 @@
+import { assert } from 'chai';
+
+import { NO_ADDITIONAL_NODES_PRESET } from '../../../../../../../src/options/presets/NoCustomNodes';
+
+import { readFileAsString } from '../../../../../../helpers/readFileAsString';
+
+import { JavaScriptObfuscator } from '../../../../../../../src/JavaScriptObfuscatorFacade';
+
+describe('BaseIdentifierObfuscatingReplacer', () => {
+    describe('Base rule', () => {
+        describe('Variant #1: default behaviour', () => {
+            let obfuscatedCode: string;
+
+            before(() => {
+                const code: string = readFileAsString(__dirname + '/fixtures/simple-input.js');
+
+                obfuscatedCode = JavaScriptObfuscator.obfuscate(
+                    code,
+                    {
+                        ...NO_ADDITIONAL_NODES_PRESET,
+                        reservedNames: ['[abc|ghi]']
+                    }
+                ).getObfuscatedCode();
+            });
+
+            it('Should keep reserved names without transformations when `reservedNames` option is enabled', () => {
+                assert.match(
+                    obfuscatedCode,
+                    /var *abc *= *0x1; *var *_0x([a-f0-9]){4,6} *= *0x2; *var *ghi *= *0x3;/
+                );
+            });
+        });
+    });
+});
---
+++ test/functional-tests/node-transformers/obfuscating-transformers/literal-transformer/obfuscating-replacers/identifier-obfuscating-replacers/fixtures/simple-input.js
@@ -0,0 +1,5 @@
+(function () {
+    var abc = 1;
+    var def = 2;
+    var ghi = 3;
+})();

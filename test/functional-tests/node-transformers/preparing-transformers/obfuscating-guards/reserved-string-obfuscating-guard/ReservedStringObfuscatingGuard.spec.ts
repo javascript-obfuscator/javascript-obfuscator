05b676e526e956b6a9e934b1fa2a36c50aaae821
sanex3339 <yarabotayuvyandex3339>
2020-01-02 15:58:35 +0300

    Added additional test for ReservedStringObfuscatingGuard

--- test/functional-tests/node-transformers/preparing-transformers/obfuscating-guards/reserved-string-obfuscating-guard/ReservedStringObfuscatingGuard.spec.ts
+++ test/functional-tests/node-transformers/preparing-transformers/obfuscating-guards/reserved-string-obfuscating-guard/ReservedStringObfuscatingGuard.spec.ts
@@ -8,7 +8,7 @@ import { readFileAsString } from '../../../../../helpers/readFileAsString';
 
 describe('ReservedStringObfuscatingGuard', () => {
     describe('check', () => {
-        describe('`\'use strict\';` operator', () => {
+        describe('`reservedString` option is enabled', () => {
             const obfuscatingGuardRegExp: RegExp = /var test1 *= *'foo' *\+ *'foo'; *var test2 *= *'barbar'; *var test3 *= *'baz' *\+ *'baz';/;
 
             let obfuscatedCode: string;
@@ -28,6 +28,30 @@ describe('ReservedStringObfuscatingGuard', () => {
             });
 
             it('match #1: shouldn\'t obfuscate reserved strings', () => {
+                assert.match(obfuscatedCode, obfuscatingGuardRegExp);
+            });
+        });
+
+        describe('`reservedString` option is disabled', () => {
+            const obfuscatingGuardRegExp: RegExp = /var test1 *= *'foo' *\+ *'foo'; *var test2 *= *'bar' *\+ *'bar'; *var test3 *= *'baz' *\+ *'baz';/;
+
+            let obfuscatedCode: string;
+
+            beforeEach(() => {
+                const code: string = readFileAsString(__dirname + '/fixtures/base-behaviour.js');
+
+                obfuscatedCode = JavaScriptObfuscator.obfuscate(
+                    code,
+                    {
+                        ...NO_ADDITIONAL_NODES_PRESET,
+                        reservedStrings: [],
+                        splitStrings: true,
+                        splitStringsChunkLength: 3
+                    }
+                ).getObfuscatedCode();
+            });
+
+            it('match #1: should obfuscate all strings', () => {
                 assert.match(obfuscatedCode, obfuscatingGuardRegExp);
             });
         });

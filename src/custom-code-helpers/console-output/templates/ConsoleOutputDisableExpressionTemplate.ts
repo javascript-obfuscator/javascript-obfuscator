3d232dda1710583a0197f975b4cc1e650fcc0cdc
sanex3339 <yarabotayuvyandex3339>
2020-08-04 22:31:03 +0300

    Slightly changed `ConsoleOutputDisableExpressionTemplate`

--- src/custom-code-helpers/console-output/templates/ConsoleOutputDisableExpressionTemplate.ts
+++ src/custom-code-helpers/console-output/templates/ConsoleOutputDisableExpressionTemplate.ts
@@ -9,9 +9,9 @@ export function ConsoleOutputDisableExpressionTemplate (): string {
             const _console = (that.console = that.console || {});
             const methods = ['log', 'warn', 'info', 'error', 'exception', 'table', 'trace'];
             
-            for (var i = 0; i < methods.length; i++){
-                const func = {consoleLogDisableFunctionName}.constructor();
-                const methodName = methods[i];
+            for (let index = 0; index < methods.length; index++){
+                const func = {callControllerFunctionName}.constructor();
+                const methodName = methods[index];
                 const originalFunction = _console[methodName] || func;
 
                 func.toString = originalFunction.toString.bind(originalFunction);
